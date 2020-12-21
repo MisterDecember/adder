1640a2898a07ce0d7e90123f4a30937570196cfe
M Azizi <Nadime.azizi@gmail.com>
2020-12-20 22:16:30 -0500

    learning and practicing with arithmetic statements

---
+++ Adder/Arithmetic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+x = 5
+y = 3
+
+print("Now let's get mathematical!")
+print()
+print('Here is your the x ({}) / y ({}) looks like since Python 3'.format(x, y))
+z = x / y
+print(f'result is {z}')
+print('______________________________________')
+print()
+
+# *********************************
+print('to get no remainder use double slash x ({}) // y ({}) '.format(x, y))
+z = x // y
+print(f'result is {z}')
+print('______________________________________')
+print()
+
+# *********************************
+print('to get ONLY remainder use percentile x ({}) % y ({}) '.format(x, y))
+z = x % y
+print(f'result is {z}')
+print('______________________________________')
+print()
