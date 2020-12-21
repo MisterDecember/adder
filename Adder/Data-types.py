a8fd56de640c6bed18492694c1bd2b7d0121e0e5
M Azizi <Nadime.azizi@gmail.com>
2020-12-20 20:57:40 -0500

    practicing with data types

--- Adder/Data-types.py
+++ Adder/Data-types.py
@@ -19,3 +19,14 @@ x = 'seven {1} {0}'.format(8, 9)
 print('This is an example of using the format function to insert value in output, but controlling the order in which they appear')
 print('x is {}'.format(x))
 print('_______________________________')
+print()
+# ***************************************
+a = 8
+b = 9
+x = f'seven {a} {b}'
+
+print('Starting in Python 3.6 the f-type is another way to format strings')
+print("For instance x = f'seven {a} {b}'")
+print('x is {}'.format(x))
+print('_______________________________')
+print()
